6efc5b8187ac57b81a1ce9042237692ac6c5aeba
KacperLasiak1 <114615725+KacperLasiak1@users.noreply.github.com>
2025-01-10 18:50:55 +0100

    Update artist_booking.py
    docu

--- artist_booking.py
+++ artist_booking.py
@@ -1,28 +1,51 @@
 def booking(artists):
-    budget = int(input("Specify your budget: "))
-    number_of_artists = int(input("Specify number of artists: "))
-    sorted_artists = artists.sort_values(by='Total Price [$]', ascending = False)
+    """
+    Function to select a specified number of artists within a given budget.
+    
+    Parameters:
+    - artists (DataFrame): A DataFrame containing artist information with columns 'artist_name' and 'Total Price [$]'.
 
-    selected_artists = []
-    remaining_budget = budget
+    Returns:
+    - selected_artists (list): A list of dictionaries with selected artist names and their corresponding total prices.
+    """
+
+    # Request budget from the user
+    budget = int(input("Specify your budget: "))
+
+    # Request the number of artists to be booked
+    number_of_artists = int(input("Specify number of artists: "))
+
+    # Sort artists by total price in descending order
+    sorted_artists = artists.sort_values(by='Total Price [$]', ascending=False)
+
+    selected_artists = []  # List to store selected artists
+    remaining_budget = budget  # Remaining budget after selecting artists
 
     while len(selected_artists) < number_of_artists and not sorted_artists.empty:
+        # Check if the top artist can be booked within the remaining budget
         if sorted_artists.iloc[0]['Total Price [$]'] <= remaining_budget:
+            # Add artist to selected list
             selected_artists.append({
                 'artist_name': sorted_artists.iloc[0]['artist_name'],
                 'total_price': int(sorted_artists.iloc[0]['Total Price [$]'])
             })
+            # Update the remaining budget
             remaining_budget -= sorted_artists.iloc[0]['Total Price [$]']
+            # Remove the selected artist from the sorted list
             sorted_artists = sorted_artists.drop(sorted_artists.index[0])
         else:
+            # If the artist exceeds budget, remove them
             sorted_artists = sorted_artists.drop(sorted_artists.index[0])
 
+        # Break the loop if no artists are left to select
         if sorted_artists.empty:
             print(f"Your budget is too small to book {number_of_artists} artists...")
             selected_artists.clear()
             break
 
+        # Check if the required number of artists has been selected
         if len(selected_artists) == number_of_artists:
+            # Calculate the total price of selected artists
             total_price = sum(artist['total_price'] for artist in selected_artists)
             print("Selected artists:")
             for artist in selected_artists:
@@ -30,5 +53,3 @@ def booking(artists):
             print(f"Total Price of Selected Artists: {total_price}")
 
     return selected_artists
-
-
